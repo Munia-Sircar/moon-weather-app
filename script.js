02f2bc19e8aad4162d19599e311d08d236184109
Munia Sircar <moonish.nutcase1991@gmail.com>
2025-08-05 12:57:59 -0500

    added searchCity function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert("hello");
+function searchCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = city.value;
+}
+
+let enterCity = document.querySelector(".search-function");
+enterCity.addEventListener("submit", searchCity);
