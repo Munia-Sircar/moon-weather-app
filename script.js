63a0ccb846af30889bbb99ea5be9c5dd08f3d41b
Munia Sircar <moonish.nutcase1991@gmail.com>
2025-08-10 11:57:29 -0500

    restructured JS for the current weather

--- script.js
+++ script.js
@@ -1,39 +1,43 @@
-function searchCity(event) {
-  event.preventDefault();
+function displayCity(response) {
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = response.data.city;
 
-  function displayCity(response) {
-    let h1 = document.querySelector("h1");
-    h1.innerHTML = response.data.city;
+  let temp = document.querySelector("#temp-now");
+  let tempNow = Math.round(response.data.temperature.current);
+  temp.innerHTML = tempNow;
 
-    let temp = document.querySelector("#temp-now");
-    let tempNow = Math.round(response.data.temperature.current);
-    temp.innerHTML = tempNow;
+  let humidity = document.querySelector("#humidity");
+  let humidityNow = Math.round(response.data.temperature.humidity);
+  humidity.innerHTML = humidityNow;
 
-    let humidity = document.querySelector("#humidity");
-    let humidityNow = Math.round(response.data.temperature.humidity);
-    humidity.innerHTML = humidityNow;
+  let wind = document.querySelector("#windspeed");
+  let windNow = Math.round(response.data.wind.speed);
+  wind.innerHTML = windNow;
 
-    let wind = document.querySelector("#windspeed");
-    let windNow = Math.round(response.data.wind.speed);
-    wind.innerHTML = windNow;
+  let description = document.querySelector("#current-description");
+  let descriptionNow = response.data.condition.description;
+  description.innerHTML = descriptionNow;
 
-    let description = document.querySelector("#current-description");
-    let descriptionNow = response.data.condition.description;
-    description.innerHTML = descriptionNow;
+  let icon = document.querySelector("#icon");
+  let iconNow = response.data.condition.icon_url;
+  icon.innerHTML = `<img
+    src="${iconNow}"
+    alt=""
+    class="current-icon" />`;
+}
 
-    let icon = document.querySelector("#icon");
-    let iconNow = response.data.condition.icon_url;
-    icon.innerHTML = `<img
-                src="${iconNow}"
-                alt=""
-                class="current-icon" />`;
-  }
-
-  let apiCity = document.querySelector("#search-input");
+function searchCity(city) {
   let apiKey = "28d0bff0ffaa52b09e33da6etode543b";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${apiCity.value}&key=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayCity);
+}
+
+function handleSearch(event) {
+  event.preventDefault();
+  let apiCity = document.querySelector("#search-input");
+
+  searchCity(apiCity.value);
 
   let now = new Date();
   let days = [
@@ -61,4 +65,6 @@ function searchCity(event) {
 }
 
 let enterCity = document.querySelector("#search-function");
-enterCity.addEventListener("submit", searchCity);
+enterCity.addEventListener("submit", handleSearch);
+
+searchCity("Dhaka");
